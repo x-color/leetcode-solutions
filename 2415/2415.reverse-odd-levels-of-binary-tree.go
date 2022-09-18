4fc1370e9a879da301d6e91c4db445cec59b9010
x-color <36035885+x-color@users.noreply.github.com>
2022-09-18 13:39:11 +0900

    Update solutions

--- 2415/2415.reverse-odd-levels-of-binary-tree.go
+++ 2415/2415.reverse-odd-levels-of-binary-tree.go
@@ -7,75 +7,27 @@ type TreeNode struct {
 }
 
 func reverseOddLevels(root *TreeNode) *TreeNode {
-	return build(replace(bfs(root)))
-}
-
-func bfs(node *TreeNode) []int {
-	left := 0
-	nodes := []*TreeNode{node}
-	for len(nodes[left:]) > 0 {
-		l := len(nodes[left:])
-		for i := 0; i < l; i++ {
-			n := nodes[left+i]
-			if n.Left != nil {
-				nodes = append(nodes, n.Left, n.Right)
-			}
-		}
-		left += l
-	}
-
-	rt := []int{}
-	for _, v := range nodes {
-		rt = append(rt, v.Val)
-	}
-
-	return rt
-}
-
-func build(nodes []int) *TreeNode {
-	root := &TreeNode{
-		Val: nodes[0],
-	}
-	nodes = nodes[1:]
-	treeNodes := []*TreeNode{root}
+	level := 0
+	nodes := []*TreeNode{root}
 	for len(nodes) > 0 {
-		i := 0
-		l := len(treeNodes)
-		for j := 0; j < l; j++ {
-			treeNodes[j].Left = &TreeNode{
-				Val: nodes[i],
+		if level%2 == 1 {
+			l, r := 0, len(nodes)-1
+			for l < r {
+				nodes[l].Val, nodes[r].Val = nodes[r].Val, nodes[l].Val
+				l++
+				r--
 			}
-			treeNodes[j].Right = &TreeNode{
-				Val: nodes[i+1],
-			}
-			i += 2
-			treeNodes = append(treeNodes, treeNodes[j].Left, treeNodes[j].Right)
 		}
-		treeNodes = treeNodes[l:]
-		nodes = nodes[l*2:]
-	}
 
+		length := len(nodes)
+		for i := 0; i < length; i++ {
+			if nodes[i].Left == nil {
+				break
+			}
+			nodes = append(nodes, nodes[i].Left, nodes[i].Right)
+		}
+		nodes = nodes[length:]
+		level++
+	}
 	return root
-}
-
-func replace(nodes []int) []int {
-	d := 1
-	l, r := 0, 0
-	for int(1<<d) < len(nodes) {
-		l += int(1 << (d - 1))
-		r += int(1 << d)
-		if d%2 == 0 {
-			d++
-			continue
-		}
-		for i := 0; i < (r-l+1)/2; i++ {
-			tmp := nodes[l+i]
-			nodes[l+i] = nodes[r-i]
-			nodes[r-i] = tmp
-		}
-
-		d++
-	}
-
-	return nodes
 }
--- 54/54.spiral-matrix.go
+++ 54/54.spiral-matrix.go
@@ -9,46 +9,36 @@ package leetcode
 // @lc code=start
 func spiralOrder(matrix [][]int) []int {
 	rt := make([]int, 0, len(matrix)*len(matrix[0]))
-	x, y, dict := 0, 0, 1
+
+	x, y := 0, 0
+	rt = append(rt, matrix[y][x])
+	matrix[y][x] = -101
 	for {
-		rt = append(rt, matrix[y][x])
-		matrix[y][x] = -101
-		x, y, dict = next(x, y, dict, matrix)
-		if dict == 0 {
+		sx, sy := x, y
+		for x+1 < len(matrix[0]) && matrix[y][x+1] > -101 {
+			x++
+			rt = append(rt, matrix[y][x])
+			matrix[y][x] = -101
+		}
+		for y+1 < len(matrix) && matrix[y+1][x] > -101 {
+			y++
+			rt = append(rt, matrix[y][x])
+			matrix[y][x] = -101
+		}
+		for x-1 >= 0 && matrix[y][x-1] > -101 {
+			x--
+			rt = append(rt, matrix[y][x])
+			matrix[y][x] = -101
+		}
+		for y-1 >= 0 && matrix[y-1][x] > -101 {
+			y--
+			rt = append(rt, matrix[y][x])
+			matrix[y][x] = -101
+		}
+		if x == sx && y == sy {
 			return rt
 		}
 	}
-}
-
-func next(x, y, dict int, matrix [][]int) (int, int, int) {
-	switch dict {
-	case 1:
-		if x+1 < len(matrix[0]) && matrix[y][x+1] > -101 {
-			return x + 1, y, 1
-		}
-		fallthrough
-	case 2:
-		if y+1 < len(matrix) && matrix[y+1][x] > -101 {
-			return x, y + 1, 2
-		}
-		fallthrough
-	case 3:
-		if x-1 >= 0 && matrix[y][x-1] > -101 {
-			return x - 1, y, 3
-		}
-		fallthrough
-	case 4:
-		if y-1 >= 0 && matrix[y-1][x] > -101 {
-			return x, y - 1, 4
-		}
-		fallthrough
-	default:
-		if x+1 < len(matrix[0]) && matrix[y][x+1] > -101 {
-			return x + 1, y, 1
-		}
-	}
-
-	return x, y, 0
 }
 
 // @lc code=end
