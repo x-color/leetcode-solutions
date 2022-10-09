a95aeb0d7bc264a97aaf1ab252ae2c5fa237c627
x-color <36035885+x-color@users.noreply.github.com>
2022-10-09 17:09:41 +0900

    Update solution

--- 148/148.sort-list.go
+++ 148/148.sort-list.go
@@ -20,33 +20,84 @@ type ListNode struct {
  * }
  */
 
-// This is an invalid answer
-// It causes Time Limit Exceeded (Time=O(n^2))
 func sortList(head *ListNode) *ListNode {
-	root := &ListNode{Next: head}
-	for n := root; n.Next != nil; n = n.Next {
-		for m := n.Next; m.Next != nil; m = m.Next {
-			// Swap Nodes (not only nodes' value)
-			// n.Next <-> m.Next
-			if n.Next.Val > m.Next.Val {
-				nn := n.Next
-				mnn := m.Next.Next
-				if nn.Next == m.Next {
-					n.Next = m.Next
-					m.Next = m.Next.Next
-					n.Next.Next = nn
-					m = n.Next
-				} else {
-					n.Next = m.Next
-					m.Next.Next = nn.Next
-					m.Next = nn
-					nn.Next = mnn
-				}
-			}
+	if head == nil {
+		return nil
+	}
+	return mergeSort(head)
+}
+
+func split(head *ListNode) (*ListNode, *ListNode) {
+	var tmp *ListNode
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		tmp = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	tmp.Next = nil
+
+	return head, slow
+}
+
+func mergeSort(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	a, b := split(head)
+	a = mergeSort(a)
+	b = mergeSort(b)
+	root := &ListNode{
+		Next: nil,
+	}
+	cur := root
+	for a != nil && b != nil {
+		if a.Val < b.Val {
+			cur.Next = a
+			a = a.Next
+		} else {
+			cur.Next = b
+			b = b.Next
 		}
+		cur = cur.Next
+	}
+
+	if a != nil {
+		cur.Next = a
+	} else {
+		cur.Next = b
 	}
 
 	return root.Next
 }
 
 // @lc code=end
+
+// This is an invalid answer
+// It causes Time Limit Exceeded (Time=O(n^2))
+// func sortList(head *ListNode) *ListNode {
+// 	root := &ListNode{Next: head}
+// 	for n := root; n.Next != nil; n = n.Next {
+// 		for m := n.Next; m.Next != nil; m = m.Next {
+// 			// Swap Nodes (not only nodes' value)
+// 			// n.Next <-> m.Next
+// 			if n.Next.Val > m.Next.Val {
+// 				nn := n.Next
+// 				mnn := m.Next.Next
+// 				if nn.Next == m.Next {
+// 					n.Next = m.Next
+// 					m.Next = m.Next.Next
+// 					n.Next.Next = nn
+// 					m = n.Next
+// 				} else {
+// 					n.Next = m.Next
+// 					m.Next.Next = nn.Next
+// 					m.Next = nn
+// 					nn.Next = mnn
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return root.Next
+// }
